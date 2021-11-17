063c87c2c734f3884bbb95a8bfc6eca9c3902771
Jesus Flores <jesus4flores@gmail.com>
2021-11-16 20:50:09 -0800

    firstCommit
    finished the assessment

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 console.log('JavaScript code has loaded!')
 
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Jesus Flores" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Gingerbread
@@ -14,10 +14,79 @@ document.getElementById('credit').textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
 document.getElementById('add-gb').addEventListener('click', function() {
+
+{
     // HINT: You can delete this console.log after you no longer need it!
     console.log('Gingerbread + button was clicked!')
 
     // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+        gb++
+        document.querySelector('#qty-gb').textContent = gb
+        myTotal()
+    }
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+document.getElementById('minus-gb').addEventListener('click', function()
+ {
+    {
+        // HINT: You can delete this console.log after you no longer need it!
+        console.log('Gingerbread + button was clicked!')
+    
+        // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+            if(gb > 0)
+            {
+              gb--
+              document.querySelector('#qty-gb').textContent = gb
+              myTotal()
+            }
+        }
+})
+
+document.getElementById('add-cc').addEventListener('click', function()
+{
+    {
+        // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+            cc++
+            document.querySelector('#qty-cc').textContent = cc
+            myTotal()
+        }
+    })
+
+    document.getElementById('minus-cc').addEventListener('click', function()
+ {
+    {
+            if(cc > 0)
+            {
+              cc--
+              document.querySelector('#qty-cc').textContent = cc
+              myTotal()
+            }
+        }
+})
+
+document.getElementById('add-sugar').addEventListener('click', function()
+{
+    {
+        // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+            sugar++
+            document.querySelector('#qty-sugar').textContent = sugar
+            myTotal()
+        }
+    })
+
+    document.getElementById('minus-sugar').addEventListener('click', function()
+ {
+    {
+            if(sugar > 0)
+            {
+              sugar--
+              document.querySelector('#qty-sugar').textContent = sugar
+              myTotal()
+            }
+        }
+})
+function myTotal()
+{
+    total = gb + cc + sugar
+    document.querySelector('#qty-total').textContent = total
+}
